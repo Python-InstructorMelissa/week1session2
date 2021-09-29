97ce7d16973b4751a07816c5d99ccfdfdc6a1924
melissa-24 <melissalongenberger24@gmail.com>
2021-09-29 02:33:04 -0400

    adding base file

---
+++ main.py
@@ -0,0 +1,9 @@
+# Know you need a function but don't know how you want it to go yet but still need to run your python file?
+ 
+def nextFunction():
+    pass
+
+"""
+Through out the next 2 months we are going to build ourselves a zoo.  Lets get our brainstorming started while working with functions
+
+"""
